86149e11ecaea59d179618243b1c6f77ebf9950c
BadBed <34012398+BadBed@users.noreply.github.com>
2018-03-04 23:29:00 +0300

    Add files via upload

---
+++ C/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "main.h"
+int main(int argc, char** argv)
+{
+	std::cout << x + y << std::endl;
+	return 0;
+}
---
+++ C/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN
+#define _MAIN
+
+#include "../A/index.h"
+#include "../B/lib.h"
+
+#endif
